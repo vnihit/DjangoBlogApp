957ebf752bc892bcbcc6660d4a7c2e1d17f1e882
Vyas <nnhv@f8ffc22e66a9.ant.amazon.com>
2021-01-29 12:10:11 +1100

    version 3

--- post/views.py
+++ post/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    print("hello world")
+    print("hello world 3")
     return HttpResponse('HELLO FROM POST')
